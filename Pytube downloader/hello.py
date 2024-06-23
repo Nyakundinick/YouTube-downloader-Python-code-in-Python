1707903beaa3324f1be04f63b40a7b8266597bf6
Nyakundinick <nonchoka1@gmail.com>
2024-06-23 22:09:31 +0300

    Add files via upload

---
+++ Pytube downloader/hello.py
@@ -0,0 +1,8 @@
+from pytube import YouTube
+
+link = input("Enter the URL of the video: ")
+video = YouTube(link)
+stream = video.streams.get_highest_resolution()
+stream.download()
+#pip install pytube in the terminal for your code to run
+#Paste the link in the terminal and hit enter(THE VIDEO WILL BE SAVED TO YOU CODE FOLDER)
